623d3873f352c45f62be02c5b09738a8324407e3
Juan Rey <juanjo9841@gmail.com>
2026-09-23 10:19:10 -0500

    Create stop.sh

---
+++ backend/z/dev/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Stopping dev env..."
+
+docker compose -f ./postgres/docker-compose.yaml down
+docker compose -f ./pulsar/docker-compose.yaml down
+docker compose -f ./seaweed/docker-compose.yaml down
+docker compose -f ./redis/docker-compose.yaml down
+
+docker network rm global-infra-net 2>/dev/null || true
+
+echo "Dev env stopped."
